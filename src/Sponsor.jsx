e832d470d60ebdb4b8db08cf95b9374bfd1f2596
carsonwolber <ctw54@cornell.edu>
2025-04-27 06:49:38 -0400

    spon content

--- src/Sponsor.jsx
+++ src/Sponsor.jsx
@@ -3,6 +3,25 @@ import './Sponsor.css'
 function Sponsor() {
   return (
     <>
+      <div className='sponsorPageBody'>
+        <div className='sponsorPageContent'>
+          <div className='textHeader'>
+            <h3>Thank You to Our Sponsors! </h3>
+          </div>
+          <div className='sponGrid'>
+            <div className='spon'>
+              <img src='dassault_logo.png' />
+            </div>
+            <div className='spon'>
+              <img src='pfp_logo.png' />
+            </div>
+          </div>
+
+
+        </div>
+
+
+      </div>
 
     </>
   )
